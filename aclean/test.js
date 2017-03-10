c9e0a84e0a749957a793b9fd609be673bc7ea7e9
zilberg <zilberg15@yandex.ru>
2017-03-10 15:08:47 +0300

    мое решение

--- aclean/test.js
+++ aclean/test.js
@@ -1,5 +1,24 @@
-describe("", function(){
-	it("", function(){
-		assert.equal(,);
+function intersection(arr1, arr2){
+	return arr1.filter(function(item){
+		return arr2.indexOf(item) != 1;
+	});
+}
+describe("содержит ровно по 1 слову из каждого набора анаграмм", function(){
+	
+	it("test 1", function(){
+		var arr = ["воз", "киборг", "корсет", "зов", "гробик", "костер", "сектор"];
+		
+		var result = aclean(arr);
+		assert.equal(result.length,3);
+		
+		assert.equal(intersection(result, ["гробик", "киборг"]).length, 1);
+		assert.equal(intersection(result, ["воз", "зов"]).length, 1);
+		assert.equal(intersection(result, ["корсет", "сектор", "костер"]).length, 1);
+		
+	});
+	
+	it("не различает регистр символов",function(){
+		var arr = ['воз','ЗОВ'];
+		assert.equal(aclean(arr).length,1);
 	});
 });
